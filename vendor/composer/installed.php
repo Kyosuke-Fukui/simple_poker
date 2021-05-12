a3d1091b0aa4709020defdcfbbb7d52678236f20
Kyosuke-Fukui <fs72os.kyosuke@gmail.com>
2021-05-13 06:26:15 +0900

    setting files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'de0d0d9f209308c2c78f1d31e2c33355f98a10d4',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'de0d0d9f209308c2c78f1d31e2c33355f98a10d4',
+    ),
+  ),
+);
